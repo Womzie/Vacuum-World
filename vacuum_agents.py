738359551a5ec8516af46d6eb11b81787c118333
Womzie <2PGIe1t4%Blw>
2022-02-18 14:37:19 -0800

    can't go backwards

--- vacuum_agents.py
+++ vacuum_agents.py
@@ -18,6 +18,8 @@ def reflex_agent(percept):
 
 def random_agent(percept):
     move = ['north', 'east', 'south','west']
+
+
     if percept == True:
         return 'clean'
     else:
@@ -27,15 +29,25 @@ def state_agent(percept):
     global i
     global last_move
     move = ['north', 'east', 'south','west']
-
+    if last_move == 'north':
+        opposite = 'south'
+    elif last_move == 'east':
+        opposite = 'west'
+    elif last_move == "south":
+        opposite = "north"
+    else:
+        opposite = "east"
 
     if percept == True:
         last_move = 'clean'
         return 'clean'
     else:
         if last_move != 'clean':
-            last_move = random.choice(move)
+            temp_move = random.choice(move)
+            while temp_move == opposite:
+                temp_move = random.choice(move)
 
+            last_move = temp_move
             return last_move
         i += 1
         last_move = move[i % 4]
@@ -43,11 +55,11 @@ def state_agent(percept):
         return last_move
 
 
-run(20, 50000, state_agent)
+#run(20, 50000, state_agent)
 
 #print(many_runs(20, 50000, 10, reflex_agent))
 
-print(many_runs(20, 50000, 10, random_agent))
+print(many_runs(20, 50000, 10, random_agent)) # hovers around 60,000
 
 print(many_runs(20, 50000, 10, state_agent))
 
