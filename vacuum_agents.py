fe2ce00973873eca56d1b42d3b6bb470f4341f65
Womzie <2PGIe1t4%Blw>
2022-02-18 13:55:29 -0800

    this sucks

--- vacuum_agents.py
+++ vacuum_agents.py
@@ -2,12 +2,14 @@
 from vacuum import *
 import random
 
-global last_percept
-global last_move
 
+last_move = 'create'
+location = [0,0]
+i = 0
 
 def reflex_agent(percept):
     if percept == True:
+
         return 'clean'
     else:
         return 'north'
@@ -22,10 +24,30 @@ def random_agent(percept):
         return random.choice(move)
 
 def state_agent(percept):
-    return 'clean'
+    global i
+    global last_move
+    move = ['north', 'east', 'south','west']
 
-#run(20, 50000, random_agent)
 
-print(many_runs(20, 50000, 10, reflex_agent))
+    if percept == True:
+        last_move = 'clean'
+        return 'clean'
+    else:
+        if last_move != 'clean':
+            last_move = random.choice(move)
+
+            return last_move
+        i += 1
+        last_move = move[i % 4]
+
+        return last_move
+
+
+run(20, 50000, state_agent)
+
+#print(many_runs(20, 50000, 10, reflex_agent))
 
 print(many_runs(20, 50000, 10, random_agent))
+
+print(many_runs(20, 50000, 10, state_agent))
+
